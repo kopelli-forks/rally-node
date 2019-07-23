09a47f35a2ece06a24ad754dc407e81f8e18ff5a
Steven Evans <kopelli@gmail.com>
2019-07-23 15:41:37 -0400

    Convert first file over to TypeScript

--- lib/util/callbackify.ts
+++ lib/util/callbackify.ts
@@ -1,4 +1,4 @@
-export default function(promise, callback) {
+export default function(promise: Promise<any>, callback: any) {
   if (typeof callback === 'function') {
     promise.then(obj => callback(null, obj), err => callback(err, null));
   }
